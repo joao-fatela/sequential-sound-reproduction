d15e6635baeae1be33c0ed4b4e27054a96e820db
João Fatela <joao.garrettfatela@unicampania.it>
2024-11-22 15:09:37 +0100

    ensure requirement install

--- sequential_sound_reproduction.bat
+++ sequential_sound_reproduction.bat
@@ -10,6 +10,9 @@ for /f "tokens=1,2 delims== " %%a in (%CONFIG_DIR%%CONFIG_FILE%) do (
 	if %%a==python_path set PYTHON_PATH=%%b
 )
 
+rem force dependency install
+"%PYTHON_PATH%" "python -m pip install -q -r requirements.txt"
+
 rem prompt user to define output device ID list
 set /p YN="Set audio device ID list? (Y/[N]): "
 
